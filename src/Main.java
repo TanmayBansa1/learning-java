5174129c266986db54a474fa35a077ec886afca9
tanmay bansal <121656091+TanmayBansa1@users.noreply.github.com>
2025-05-21 08:49:40 +0530

    optional+map

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,27 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class Main{
     public static void main(String[]args){
 
         System.out.println("Hello World");
-        List<Object> list = new ArrayList<Object>();
+        List<Object> list = new ArrayList<>();
 
         list.add("Hello");
         list.add(543);
         list.add("World");
+        Map<String,Integer> map = new HashMap<>();
+        map.put("first", 25);
 
-
-        for(int i=0; i<list.size(); i++ ){
-            System.out.println(list.get(i));
-            System.out.println("Main.main");
-            System.out.println("args = " + Arrays.toString(args));
-            System.out.println("i = " + i);
+        for (Object o : list) {
+            System.out.println(o);
         }
+            map.forEach((k, v) -> {
+                System.out.println("key:" + k + " value:" + v);
+            });
+
+        Optional<Integer> marks = Optional.ofNullable(map.get("first"));
+        System.out.println(marks);
+        marks.ifPresent(System.out::println);
+
     }
 }
