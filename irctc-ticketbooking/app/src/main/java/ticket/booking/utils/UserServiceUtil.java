0fbee509580ed1b190b05090ca17f1cf29f04935
tanmay bansal <121656091+TanmayBansa1@users.noreply.github.com>
2025-05-23 03:45:21 +0530

    checkpassword function

--- irctc-ticketbooking/app/src/main/java/ticket/booking/utils/UserServiceUtil.java
+++ irctc-ticketbooking/app/src/main/java/ticket/booking/utils/UserServiceUtil.java
@@ -1,4 +1,12 @@
 package ticket.booking.utils;
 
+
+import org.mindrot.jbcrypt.BCrypt;
+
 public class UserServiceUtil {
+
+    public static Boolean checkPassword(String password, String hashedPassword){
+
+        return BCrypt.checkpw(password,hashedPassword);
+    }
 }
