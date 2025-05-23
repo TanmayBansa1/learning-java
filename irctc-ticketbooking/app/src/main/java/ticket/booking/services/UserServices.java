89d0cad6aab7a8db1a711f928675f052e0c17a80
tanmay bansal <121656091+TanmayBansa1@users.noreply.github.com>
2025-05-23 17:13:35 +0530

    added fetch  booking and cancel booking

--- irctc-ticketbooking/app/src/main/java/ticket/booking/services/UserServices.java
+++ irctc-ticketbooking/app/src/main/java/ticket/booking/services/UserServices.java
@@ -2,13 +2,17 @@ package ticket.booking.services;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import ticket.booking.entities.Ticket;
 import ticket.booking.entities.User;
 import ticket.booking.utils.UserServiceUtil;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class UserServices {
 
@@ -25,11 +29,57 @@ public class UserServices {
 
     public Boolean UserLogin(){
         Optional<User> foundUser = listOfusers.stream().filter(user1->{
-            return (user1.getName()).equals(user.getName()) && UserServiceUtil.checkPassword(user1.getPassword(),user.getHashedPassword());
+            return (user1.getName()).equalsIgnoreCase(user.getName()) && UserServiceUtil.checkPassword(user.getPassword(),user1.getHashedPassword());
         }).findFirst();
         return foundUser.isPresent();
 
     }
     public void UserLogout(User user){}
+
+    public Boolean signup(User user){
+        try{
+            listOfusers.add(user);
+            saveUsertoFile();
+            return true;
+        }catch (IOException e){
+            return false;
+        }
+    }
+
+    public void saveUsertoFile() throws IOException {
+        try{
+            File users = new File(USERS_PATH);
+            objectMapper.writeValue(users, listOfusers);
+        }catch (IOException e){
+            System.out.println(e.getMessage());
+            throw e;
+        }
+    }
+
+    public List<Ticket> fetchBookings(){
+        return user.getTicketsBooked();
+    }
+
+    public Boolean cancelTicket(){
+        Scanner s = new Scanner(System.in);
+        System.out.println("Please enter the id of the ticket you wish to cancel: ");
+        String ticketId = s.next();
+        List<Ticket> tickets = fetchBookings();
+
+        boolean removed = tickets.removeIf(t ->
+             t.getTicketId().equals(ticketId)
+        );
+
+        if(removed){
+            System.out.println("Ticket has been cancelled");
+            return true;
+        }else{
+            System.out.println("Ticket has not been cancelled");
+            return false;
+        }
+
+    }
+
+
 
 }
